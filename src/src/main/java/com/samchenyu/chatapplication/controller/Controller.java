9f3a988732eb94da582c77f6f5fcca6465799771
Sam Chen Yu <150127006+SamChenYu@users.noreply.github.com>
2024-07-17 16:16:36 +0800

    Add files via upload

---
+++ src/src/main/java/com/samchenyu/chatapplication/controller/Controller.java
@@ -0,0 +1,115 @@
+package com.samchenyu.chatapplication.controller;
+
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.samchenyu.chatapplication.service.MessagingService;
+import com.samchenyu.chatapplication.model.*;
+
+import java.util.List;
+
+@RestController
+@Slf4j
+@AllArgsConstructor
+@RequestMapping
+public class Controller {
+
+    private final MessagingService messagingService;
+    private final SimpMessagingTemplate simpMessagingTemplate;
+
+
+    @PostMapping("/login")
+    public ResponseEntity<List<Chat>> login(@RequestBody User user) {
+        boolean success = messagingService.getUserStorage().login(user.getUsername(), user.getPassword());
+
+        if (success) {
+            System.out.println("Login success");
+            List<Chat> chat = messagingService.getChatList(user);
+            return ResponseEntity.ok(chat);
+        } else {
+            System.out.println("Login failed");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
+    /* address: localhost:8080/login
+        sample json to send in postman
+        {
+            "username": "user1",
+            "password": "user1"
+        }
+
+
+     */
+
+
+    @PostMapping("/sendmessage")
+    public ResponseEntity<Chat> sendMessage(@RequestBody Message message) {
+        Chat chat = messagingService.sendMessage(message.getChatID(), message);
+        if (chat == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        // sends the message to the socket
+        simpMessagingTemplate.convertAndSend("/topic/" + message.getChatID(), message);
+        return ResponseEntity.ok(chat);
+    }
+
+    /* address: localhost:8080/sendmessage
+        sample json to send in postman
+        {
+            "from": "user1",
+            "recipient": "user2",
+            "text": "this is a new message",
+            "time": "12:00",
+            "chatID": "chat1"
+        }
+     */
+
+    @PostMapping("/newchat")
+    public ResponseEntity<Chat> newChat(@RequestBody ChatRequest chatRequest) {
+        User recipientUser = new User();
+        recipientUser.setUsername(chatRequest.getRecipient());
+        Chat chat = messagingService.connectToChat(chatRequest.getUser(), recipientUser);
+        return ResponseEntity.ok(chat);
+    }
+    /* address: localhost:8080/newchat
+        sample json to send in postman
+        {
+            "user": {
+                "username": "user1",
+                "password": "user1"
+            },
+            "recipient": "user2"
+        }
+     */
+
+    @PostMapping("/chatlist")
+    public ResponseEntity<List<User>> chatList(@RequestBody User user) {
+        List<User> userList = messagingService.getUserList(user);
+        return ResponseEntity.ok(userList);
+    }
+    /* address: localhost:8080/chatlist
+        sample json to send in postman
+        {
+            "username": "user1",
+            "password": "user1"
+        }
+     */
+
+
+
+
+    @PostMapping("/hello")
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("Hello World");
+    }
+    // address: localhost:8080/hello
+
+}
---
+++ src/src/main/java/com/samchenyu/chatapplication/storage/ChatStorage.java
@@ -0,0 +1,160 @@
+package com.samchenyu.chatapplication.storage;
+
+import com.samchenyu.chatapplication.model.Chat;
+import com.samchenyu.chatapplication.model.Message;
+import com.samchenyu.chatapplication.model.User;
+
+import lombok.Data;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.UUID;
+
+@Data
+public class ChatStorage {
+
+    private static List<Chat> chats = new ArrayList<>();
+    private static ChatStorage instance;
+
+
+    public static synchronized ChatStorage getInstance() {
+        if (instance == null) {
+            instance = new ChatStorage();
+            addSampleChat();
+
+        }
+        return instance;
+    }
+
+
+    public static void addSampleChat() {
+        // for testing purposes
+
+
+        // chat1 between user1 and user2
+        Chat chat1 = new Chat();
+        chat1.setChatID("chat1");
+        User user1 = new User();
+        user1.setUsername("user1");
+        user1.setPassword("user1");
+        chat1.addParticipant(user1);
+
+
+        User user2 = new User();
+        user2.setUsername("user2");
+        user2.setPassword("user2");
+        chat1.addParticipant(user2);
+
+        Message message1 = new Message();
+        message1.setFrom("user1");
+        message1.setRecipient("user2");
+        message1.setText("Hello");
+        message1.setTime("12:00");
+        message1.setChatID("chat1");
+        chat1.addMessage(message1);
+
+        Message message2 = new Message();
+        message2.setFrom("user2");
+        message2.setRecipient("user1");
+        message2.setText("Hi");
+        message2.setTime("12:01");
+        message2.setChatID("chat1");
+        chat1.addMessage(message2);
+
+        chats.add(chat1);
+
+        // chat2 between user1 and user3
+        Chat chat2 = new Chat();
+        String chatID = UUID.randomUUID().toString();
+        chat2.setChatID(chatID);
+        User user3 = new User();
+        user3.setUsername("user3");
+        user3.setPassword("user3");
+
+        chat2.addParticipant(user1);
+        chat2.addParticipant(user3);
+
+        Message message3 = new Message();
+        message3.setFrom("user1");
+        message3.setRecipient("user3");
+        message3.setText("Hello");
+        message3.setTime("12:00");
+        message3.setChatID(chatID);
+
+        chat2.addMessage(message3);
+
+        Message message4 = new Message();
+        message4.setFrom("user3");
+        message4.setRecipient("user1");
+        message4.setText("Hi");
+        message4.setTime("12:01");
+        message4.setChatID(chatID);
+
+        chat2.addMessage(message4);
+
+
+        chats.add(chat2);
+
+    }
+
+    public void addChat(Chat chat) {
+        chats.add(chat);
+    }
+
+
+    public List<Chat> getChatList(User user) {
+        List<Chat> userChats = new ArrayList<>();
+        String username = user.getUsername();
+
+        for (Chat chat : chats) {
+            for (User participant : chat.getParticipants()) {
+                if (participant.getUsername().equals(username)) {
+                    userChats.add(chat);
+                }
+            }
+        }
+        return userChats;
+    }
+
+
+    public Chat getChat(User user1, User user2) {
+
+        for (Chat chat : chats) {
+            boolean user1Found = false;
+            boolean user2Found = false;
+            for(User participant : chat.getParticipants()) {
+                if (participant.getUsername().equals(user1.getUsername())) {
+                    user1Found = true;
+                }
+                if (participant.getUsername().equals(user2.getUsername())) {
+                    user2Found = true;
+                }
+            }
+            if (user1Found && user2Found) {
+                return chat;
+            }
+        }
+
+        return newChat(user1, user2);
+    }
+
+    public Chat newChat(User user1, User user2) {
+        Chat chat = new Chat();
+        chat.setChatID(UUID.randomUUID().toString());
+        chat.getParticipants().add(user1);
+        chat.getParticipants().add(user2);
+        return chat;
+    }
+
+    public Chat getChatByID(String chatID) {
+        for (Chat chat : chats) {
+            if (chat.getChatID().equals(chatID)) {
+                return chat;
+            }
+        }
+        throw new IllegalArgumentException("Chat not found");
+    }
+
+
+
+}

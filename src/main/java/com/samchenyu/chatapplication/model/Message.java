9b009a9a27c96a65510cdd84e5ab5f1915e1f61e
Sam Chen Yu <150127006+SamChenYu@users.noreply.github.com>
2024-08-07 15:55:21 +0800

    Started porting storage objects to MySQL Entities

--- src/main/java/com/samchenyu/chatapplication/model/Message.java
+++ src/main/java/com/samchenyu/chatapplication/model/Message.java
@@ -1,17 +1,37 @@
 package com.samchenyu.chatapplication.model;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
+@Entity
+@Table(name = "message")
 @Data
 public class Message {
 
-    private String from;
-    private String text;
-    private String recipient;
-    private String time;
-    private String chatID;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "messageID")
     private int messageID; // sequential numbering of chats
+
+    @Column(name = "from")
+    private String from;
+
+    @Column(name = "text")
+    private String text;
+
+    @Column(name = "recipient")
+    private String recipient;
+
+    @Column(name = "time")
+    private String time;
+
+    @Column(name = "chatID")
+    private String chatID;
+
+    @Column(name="authToken")
     private String authToken; // authToken
 
-
+    @ManyToOne
+    @JoinColumn(name = "chatID", insertable = false, updatable = false)
+    private Chat chat;
 }

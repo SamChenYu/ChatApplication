dffc7a06cee8d24fcf3cbcc92be9e2e24854be28
Sam Chen Yu <150127006+SamChenYu@users.noreply.github.com>
2024-08-08 18:54:54 +0800

    Delete src directory

--- src/main/java/com/samchenyu/chatapplication/storage/Repository.java
+++
@@ -1,7 +0,0 @@
-package com.samchenyu.chatapplication.storage;
-
-import com.samchenyu.chatapplication.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Repository extends JpaRepository<User, Long> {
-}
